c9052b823ea1ae29f548e025609934bb14775008
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2019-07-08 17:55:37 -0700

    Created initial database models

---
+++ ThinksterASPCoreApi/DatabaseModels/Moon.cs
@@ -0,0 +1,9 @@
+﻿namespace ThinksterASPCoreApi.DatabaseModels
+{
+    public class Moon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Mass { get; set; }
+    }
+}
