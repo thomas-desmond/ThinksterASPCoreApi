8b07bb04762a57fa326a7db44ae2894b191d49cb
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2019-07-10 08:30:37 -0700

    Created Space Repository

---
+++ ThinksterASPCoreApi/PlanetDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThinksterASPCoreApi.DatabaseModels;
+
+namespace ThinksterASPCoreApi
+{
+    public class PlanetDatabaseContext : DbContext
+    {
+        public PlanetDatabaseContext(DbContextOptions<PlanetDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Planet> Planets { get; set; }
+
+        public DbSet<Moon> Moons { get; set; }
+    }
+}
