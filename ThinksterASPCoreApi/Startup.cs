dd7f70260e022e0c173c05d23f7025e15095953c
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2019-07-10 08:30:24 -0700

    Added dbcontext service to startup. Added Entity Framework packages.

--- ThinksterASPCoreApi/Startup.cs
+++ ThinksterASPCoreApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,8 @@ namespace ThinksterASPCoreApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PlanetDatabaseContext>(opt => opt.UseInMemoryDatabase());
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
